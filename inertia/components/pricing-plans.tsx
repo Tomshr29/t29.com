6c7465188b82a2098a907fa5c528337f898725e4
Tomshr29 <tomscherer29@gmail.com>
2024-10-18 18:02:27 +0200

    finish first pricing plans

--- inertia/components/pricing-plans.tsx
+++ inertia/components/pricing-plans.tsx
@@ -2,16 +2,66 @@ import { pricingPlans } from "../data";
 
 export default function PricingPlans() {
   return (
-    <div className="mx-auto grid max-w-7xl grid-cols-3 gap-8 px-4 py-24 sm:px-6 lg:px-8">
-      {pricingPlans.map((plan) => (
-        <div
-          key={plan.title}
-          className="rounded-lg border border-slate-200 bg-white p-8 shadow-lg"
-        >
-          <h3 className="text-lg font-semibold leading-5">{plan.title}</h3>
-          <p className="mt-4 text-sm text-slate-700">{plan.description}</p>
-        </div>
-      ))}
+    <div className="relative">
+      <div className="absolute inset-0 flex flex-col">
+        <div className="flex-1"></div>
+        <div className="flex-1 bg-slate-50"></div>
+      </div>
+      <div className="mx-auto grid max-w-7xl gap-12 px-4 py-24 sm:px-6 lg:grid-cols-3 lg:gap-8 lg:px-8">
+        {pricingPlans.map((plan) => (
+          <div
+            key={plan.title}
+            className="relative flex flex-col rounded-lg border border-slate-200 bg-white p-8 shadow-lg"
+          >
+            <h3 className="text-lg font-semibold leading-5">{plan.title}</h3>
+            {plan.mostPopular && (
+              <p className="absolute top-0 -translate-y-1/2 rounded-full bg-cyan-500 px-3 py-0.5 text-sm font-semibold tracking-wide text-white shadow-md">
+                Most Popular
+              </p>
+            )}
+            <p className="mt-4 text-sm leading-6 text-slate-700">
+              {plan.description}
+            </p>
+            <div className="-mx-6 mt-4 rounded-lg bg-slate-50 p-6">
+              <p className="flex items-center text-sm font-semibold text-slate-500">
+                <span>{plan.currency}</span>
+                <span className="ml-3 text-4xl text-slate-900">
+                  ${plan.price}
+                </span>
+                <span className="ml-1.5">{plan.frequency}</span>
+              </p>
+            </div>
+            <ul className="mt-6 flex-1 space-y-4">
+              {plan.features.map((feature) => (
+                <li
+                  key={feature}
+                  className="flex text-sm leading-6 text-slate-700"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="5 w-5 shrink-0 text-cyan-500"
+                  >
+                    <path d="M20 6 9 17l-5-5" />
+                  </svg>
+                  <span className="ml-3">{feature}</span>
+                </li>
+              ))}
+            </ul>
+            <a
+              href="#"
+              className={`mt-8 block rounded-lg px-6 py-4 text-center text-sm font-semibold leading-4 ${plan.mostPopular ? "bg-cyan-500 text-white shadow-md hover:bg-cyan-600" : "bg-cyan-50 text-cyan-700 hover:bg-cyan-100"} `}
+            >
+              {plan.cta}
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,34 +1,33 @@
-import { Head } from '@inertiajs/react'
-import { useState } from 'react'
-import Contact from '~/components/Contact'
-import Footer from '~/components/footer'
-import Menu from '~/components/Menu'
-import Pricing from '~/components/Pricing'
-import PricingPlans from '~/components/pricing-plans'
-import Text from '~/components/Text'
+import { Head } from "@inertiajs/react";
+import { useState } from "react";
+import Contact from "~/components/Contact";
+import Footer from "~/components/footer";
+import Menu from "~/components/Menu";
+import PricingPlans from "~/components/pricing-plans";
+import Text from "~/components/Text";
 
 const stats = [
-  { id: 1, name: 'Sites Web Lancés', value: '1 200+' },
-  { id: 2, name: 'Clients Servis', value: '350+' },
-  { id: 3, name: 'Projets Complétés', value: '750+' },
-]
+  { id: 1, name: "Sites Web Lancés", value: "1 200+" },
+  { id: 2, name: "Clients Servis", value: "350+" },
+  { id: 3, name: "Projets Complétés", value: "750+" },
+];
 
 export default function Home(props: { version: number }) {
-  const [menuOpen, setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleMenuClick = () => {
-    setMenuOpen(!menuOpen)
-  }
+    setMenuOpen(!menuOpen);
+  };
   return (
     <>
       <Head title="Homepage" />
 
-      <div className="flex justify-between py-8 px-12 absolute w-full">
-        <Text as="p" className="text-xl text-neutral-700 text-center">
+      <div className="absolute flex w-full justify-between px-12 py-8">
+        <Text as="p" className="text-center text-xl text-neutral-700">
           T29
         </Text>
         <button className="text-lg" onClick={handleMenuClick}>
-          <Text as="p" className="text-xl text-neutral-700 text-center">
+          <Text as="p" className="text-center text-xl text-neutral-700">
             Menu
           </Text>
         </button>
@@ -36,23 +35,26 @@ export default function Home(props: { version: number }) {
 
       <Menu isOpen={menuOpen} onClose={() => setMenuOpen(false)} />
 
-      <div className="h-screen flex items-center justify-start px-20 bg-black">
+      <div className="flex h-screen items-center justify-start bg-black px-20">
         <div className="w-full max-w-4xl">
           <Text
             as="h1"
-            className="text-9xl font-medium tracking-tighter text-start leading-2 text-white  mb-4"
+            className="leading-2 mb-4 text-start text-9xl font-medium tracking-tighter text-white"
           >
             Boost your website's SEO.
           </Text>
-          <Text as="h1" className="hidden text-3xl font-semibold text-start text-white mb-4">
+          <Text
+            as="h1"
+            className="mb-4 hidden text-start text-3xl font-semibold text-white"
+          >
             Design subscriptions for everyone.
           </Text>
-          <Text as="p" className="text-xl text-neutral-300 text-start">
+          <Text as="p" className="text-start text-xl text-neutral-300">
             Design subscriptions for everyone. Pause or cancel anytime.
           </Text>
-          <div className="flex w-full justify-start items-center mt-6">
-            <button className="bg-blue-600 text-white font-bold py-2.5 px-9 rounded-md shadow-lg ring-2 ring-blue-500 hover:bg-gray-800 transition duration-300 tracking-tight text-[17px]">
-              <Text as="h1" className="text-xl font-semibold text-center">
+          <div className="mt-6 flex w-full items-center justify-start">
+            <button className="rounded-md bg-blue-600 px-9 py-2.5 text-[17px] font-bold tracking-tight text-white shadow-lg ring-2 ring-blue-500 transition duration-300 hover:bg-gray-800">
+              <Text as="h1" className="text-center text-xl font-semibold">
                 Get Started
               </Text>
             </button>
@@ -60,11 +62,17 @@ export default function Home(props: { version: number }) {
         </div>
       </div>
 
-      <div className="bg-white py-24 sm:py-32 border-b border-neutral-100" id="pricing">
+      <div
+        className="border-b border-neutral-100 bg-white py-24 sm:py-32"
+        id="pricing"
+      >
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
             {stats.map((stat) => (
-              <div key={stat.id} className="mx-auto flex max-w-xs flex-col gap-y-4">
+              <div
+                key={stat.id}
+                className="mx-auto flex max-w-xs flex-col gap-y-4"
+              >
                 <Text as="dt" className="text-base leading-7 text-gray-600">
                   {stat.name}
                 </Text>
@@ -80,20 +88,20 @@ export default function Home(props: { version: number }) {
         </div>
       </div>
       <Contact />
-      <Pricing />
       <main>
         <div className="mx-auto max-w-7xl bg-white px-4 pt-24 sm:px-6 lg:px-8">
           <h2 className="text-3xl font-extrabold text-black sm:text-5xl sm:leading-tight sm:tracking-tight">
             Pricing plans for teams of all sizes
           </h2>
           <p className="mt-4 max-w-3xl text-lg text-slate-500">
-            Choose an affordable plan that's packed with the best features for engaging your
-            audience, creating customer loyalty, and driving sales.
+            Choose an affordable plan that's packed with the best features for
+            engaging your audience, creating customer loyalty, and driving
+            sales.
           </p>
         </div>
         <PricingPlans />
       </main>
       <Footer />
     </>
-  )
+  );
 }
